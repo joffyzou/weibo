b1ab11b2fdeca6cc2b694e91b5d7a1374dd62518
joffy zou <joffyzou@icloud.com>
2021-01-17 07:28:34 +0000

    User 模型的一些功能注释

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,20 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable; // 授权相关功能
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    // HasFactory 模型工程相关功能的引用
+    // Notifiable 消息通知相关功能的引用
     use HasFactory, Notifiable;
 
+    // 指定数据表名
+    protected $table = 'users';
+
     /**
+     * 允许批量赋值的字段
      * The attributes that are mass assignable.
      *
      * @var array
@@ -23,6 +29,7 @@ class User extends Authenticatable
     ];
 
     /**
+     * 当我们需要对用户密码或其他敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
      * The attributes that should be hidden for arrays.
      *
      * @var array
