3d679b06462e8c06ee80f00fb575864bcac7e255
joffy zou <joffyzou@icloud.com>
2021-01-17 09:06:41 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,27 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    // 注册页面
     public function create()
     {
         return view('users.create');
     }
 
+    // 单个用户展示页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    // 保存注册
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        return;
     }
 }
