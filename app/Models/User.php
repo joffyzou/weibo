a87ab16e164e62f7ca42d10c897851d84456c346
joffy zou <joffyzou@icloud.com>
2021-01-19 08:18:02 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,9 @@ class User extends Authenticatable
             $user->activation_token = Str::random(10);
         });
     }
+
+    public function statuses()
+    {
+        $this->hasMany(Status::class);
+    }
 }
