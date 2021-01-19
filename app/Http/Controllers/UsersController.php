a03a22e55f6f8c407ffa7f0255f2240e77164fc3
joffy zou <joffyzou@icloud.com>
2021-01-19 10:37:37 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -40,7 +40,8 @@ class UsersController extends Controller
     // 单个用户展示页面
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     // 保存注册
--- app/Models/User.php
+++ app/Models/User.php
@@ -73,6 +73,6 @@ class User extends Authenticatable
 
     public function statuses()
     {
-        $this->hasMany(Status::class);
+        return $this->hasMany(Status::class);
     }
 }
