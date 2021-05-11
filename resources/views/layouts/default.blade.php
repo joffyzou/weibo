dc59a3397a9a75626f7aac6aa4c6d82daf29c4fa
joffy <joffyzou@qq.com>
2021-05-11 09:40:41 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,6 +13,7 @@
     @include('layouts._header')
     <div class="container">
         <div class="offset-md-1 col-md-10">
+            @include('shared._messages')
             @yield('content')
             @include('layouts.footer')
         </div>
