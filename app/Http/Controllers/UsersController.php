4d82196e6c6c5c9008949b6ce855eda969d3b878
joffy <joffyzou@qq.com>
2021-05-11 11:41:02 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,14 +8,14 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
-        return view('users.create');
-    }
-
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
     }
 
     public function store(Request $request)
@@ -35,5 +35,27 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程！');
 
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = $request->password;
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
     }
 }
