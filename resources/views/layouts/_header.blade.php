fea6ef5f14987e843aab4237c6618493d11e9e9c
joffy <joffyzou@qq.com>
2021-05-11 10:29:48 +0800

    登录成功后导航逻辑

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,8 +2,29 @@
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
-            <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+            @if (Auth::check())
+                <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">用户列表</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="{{ route('users.show', Auth::user()) }}" class="dropdown-item">个人中心</a>
+                        <a href="{{ route('users.edit', Auth::user()) }}" class="dropdown-item">编辑资料</a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item" id="logout">
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" name="submit" class="btn btn-block btn-danger">退出</button>
+                            </form>
+                        </a>
+                    </div>
+                </li>
+            @else
+                <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
+                <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+            @endif
         </ul>
     </div>
 </nav>
