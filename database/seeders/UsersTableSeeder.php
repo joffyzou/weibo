096024e3b89c0f9324173f139a4d684a00bd8eff
joffy <joffyzou@qq.com>
2021-05-11 15:28:57 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'Joffy';
         $user->email = 'joffyzou@icloud.com';
         $user->password = bcrypt(123456);
+        $user->is_admin = true;
         $user->save();
     }
 }
