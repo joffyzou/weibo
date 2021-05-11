a6c66773f55602b6a1524fb933c95d5b862f9582
joffy <joffyzou@qq.com>
2021-05-11 17:54:54 +0800

    发送找回密码链接

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -49,6 +49,6 @@ class SessionsController extends Controller
         Auth::logout();
         session()->flash('success', '您已成功退出！');
 
-        return redirect('login');
+        return redirect()->back();
     }
 }
