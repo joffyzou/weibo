65b5fed8c19692d2b4ba8e044f3871178723e49a
joffy <joffyzou@qq.com>
2021-05-12 11:06:51 +0800

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -9,4 +9,11 @@
         </h5>
         {{ $status->content }}
     </div>
+    @can ('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您确定要删除本条微博吗？')">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-sm btn-danger">删除</button>
+        </form>
+    @endcan
 </li>
